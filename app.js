6c7dc5b0f973738a57f5af6152158100a20270ac
PJ Jones <pj.jones4000.com>
2022-02-10 13:45:02 -0600

    got some fields and buttons and containers

---
+++ app.js
@@ -0,0 +1,31 @@
+class Shape {
+  constructor() {}
+
+  describe() {
+    
+  }
+}
+
+class Circle extends Shape {
+  constructor() {
+    super();
+  }
+}
+
+class Triangle extends Shape {
+  constructor() {
+    super();
+  }
+}
+
+class Rectangle extends Shape {
+  constructor() {
+    super();
+  }
+}
+
+class Square extends Shape {
+  constructor() {
+    super();
+  }
+}
