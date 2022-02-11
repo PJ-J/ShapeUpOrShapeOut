e0e4540a7938611e34b126ed60a6ec5b927308fd
PJ Jones <pj.jones4000.com>
2022-02-11 10:50:02 -0600

    can't figure out how to get the shapes to register when clicked

--- app.js
+++ app.js
@@ -3,43 +3,55 @@ class Shape {
     this.height = height;
     this.width = width;
   }
-
-  describe() {}
 }
 
 class Circle extends Shape {
-  constructor(height, width) {
-    super(height, width);
+  constructor(radius) {
+    super();
 
+    this.radius = radius;
+  }
+  buildCircle() {
     let div = $("<div>");
-    div.css("background", "purple");
-    div.css("height", height);
-    div.css("width", width);
+    div.id = "circle";
+    div.css("height", this.radius);
+    div.css("width", this.radius);
     div.css("border-radius", "50%");
+    div.css("background", "purple");
     div.appendTo($("#shapes"));
+  }
+  describe() {
+    console.log("describe");
   }
 }
 $("#circleBtn").on("click", function () {
-  let c1 = new Circle($("#radius").val(), $("#radius").val());
+  let c1 = new Circle($("#radius").val());
+
+  c1.buildCircle();
 
   console.log(c1);
 });
+$("<div>").on("click", function() {
+  describe();
+});
 
 class Triangle extends Shape {
-  constructor(height, width) {
-    super(height, width);
-
+  constructor(height) {
+    super();
+    this.height = height;
+  }
+  makeTriangle() {
     let div = $("<div>");
-    // div.css("background", "yellow");
     div.css("height", "0");
     div.css("width", "0");
-    div.css("border-bottom", `${height}px solid yellow`);
-    div.css("border-right", `${height}px solid transparent`);
+    div.css("border-bottom", `${this.height}px solid yellow`);
+    div.css("border-right", `${this.height}px solid transparent`);
     div.appendTo($("#shapes"));
   }
 }
 $("#triBtn").on("click", function () {
-  let t1 = new Triangle($("#triHeight").val(), $("#triHeight").val());
+  let t1 = new Triangle($("#triHeight").val());
+  t1.makeTriangle();
 
   console.log(t1);
 });
@@ -47,33 +59,38 @@ $("#triBtn").on("click", function () {
 class Rectangle extends Shape {
   constructor(height, width) {
     super(height, width);
-
+  }
+  makeRectangle() {
     let div = $("<div>");
     div.css("background", "green");
-    div.css("height", height);
-    div.css("width", width);
+    div.css("height", this.height);
+    div.css("width", this.width);
     div.appendTo($("#shapes"));
   }
 }
 $("#rectBtn").on("click", function () {
   let r1 = new Rectangle($("#rheight").val(), $("#rwidth").val());
-
+  r1.makeRectangle();
   console.log(r1);
 });
 
 class Square extends Shape {
-  constructor(height, width) {
-    super(height, width);
-
+  constructor(sideLength) {
+    super();
+    this.sideLength = sideLength;
+  }
+  makeSquare() {
     let div = $("<div>");
     div.css("background", "red");
-    div.css("height", height);
-    div.css("width", width);
+    div.css("height", this.sideLength);
+    div.css("width", this.sideLength);
     div.appendTo($("#shapes"));
   }
 }
 $("#squBtn").on("click", function () {
   let s1 = new Square($("#sideLength").val(), $("#sideLength").val());
+
+  s1.makeSquare();
 
   console.log(s1);
 });
